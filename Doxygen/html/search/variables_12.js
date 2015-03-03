a3a1e9ba144dd3297ccf1686ebd2bcbef36245e1
Jonno Schmidt <jonathan.schmidt@duke.edu>
2015-03-03 22:13:05 +0000

    Added Readme

--- Doxygen/html/search/variables_12.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['user_5fdefined_5fcommands_5fheader',['USER_DEFINED_COMMANDS_HEADER',['../d2/d5a/classview_1_1_strings.html#a3b1d891c5332ea7ddd82c8cce9cf8ca8',1,'view::Strings']]]
-];
