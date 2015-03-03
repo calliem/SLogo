738d494a0893c7583e3f50a9d0dde29bdcccfb0b
Jonno Schmidt <jonathan.schmidt@duke.edu>
2015-03-03 22:07:57 +0000

    Added Readme

--- Doxygen/html/search/files_9.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['parser_2ejava',['Parser.java',['../d2/de6/_parser_8java.html',1,'']]],
-  ['polar_2ejava',['Polar.java',['../d5/dd9/_polar_8java.html',1,'']]],
-  ['propertiesview_2ejava',['PropertiesView.java',['../de/d85/_properties_view_8java.html',1,'']]]
-];
--- Doxygen/html/search/functions_a.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['node',['Node',['../d2/d19/classmodel_1_1_node.html#aaa54cd3b86b99f8a8e0da5b30c6080f5',1,'model::Node']]]
-];
