e2ef6c6c7a94003938174347ae4576d3f6bf980d
Jonno Schmidt <jonathan.schmidt@duke.edu>
2015-03-07 00:57:17 +0000

    Added Readme

--- Doxygen/html/search/functions_1.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['booleaninstruction',['BooleanInstruction',['../db/d32/classmodel_1_1instructions_1_1_boolean_instruction.html#a0594bec897f29e6b1e26dd0807792f26',1,'model::instructions::BooleanInstruction']]],
-  ['booltodouble',['boolToDouble',['../db/d32/classmodel_1_1instructions_1_1_boolean_instruction.html#a69da5bfa4c4f39cdf65f0fa5537e67fc',1,'model::instructions::BooleanInstruction']]]
-];
